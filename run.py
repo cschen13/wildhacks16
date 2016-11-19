e2baa8c874391eb4444be538a13d20452686882e
Kevin Wilde <kevinwilde2018@u.northwestern.edu>
2016-11-19 01:08:05 -0600

    Trying to get response to texts

--- run.py
+++ run.py
@@ -8,16 +8,17 @@ callers = set(["+12246221941"])
 @app.route("/", methods=['GET', 'POST'])
 def respond_to_message():
     from_number = request.values.get('From', None)
-    from_message = request.values.get('Body', None)
-    if from_number not in callers:
-        callers.add(from_number)
-    if from_message.lower() == "done":
-        callers.remove(from_number)
-    else:
-        message = "Monkey, thanks for the message!"
-        resp = twilio.twiml.Response()
-        resp.message(message)
-        return str(resp)
+    from_message = request.values.get('Body', '')
+    if from_number:
+        if from_number not in callers:
+            callers.add(from_number)
+        if from_message.lower() == "done":
+            callers.remove(from_number)
+        else:
+            message = "Monkey, thanks for the message!"
+            resp = twilio.twiml.Response()
+            resp.message(message)
+            return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
